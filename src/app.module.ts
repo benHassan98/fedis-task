1bd1cfeaaaeb1affcf936155a05f64031f0be141
Abdullah Mahmoud <habdala70@gmail.com>
2024-07-17 19:13:27 +0000

    added auth resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
